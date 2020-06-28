2cdee161bcf1b431893f214707821c4650ed68d9
Serdar <serdar2225@gmail.com>
2020-06-28 19:30:37 -0400

    1st hover over

--- src/main/java/hoverOverMethod.java
+++ src/main/java/hoverOverMethod.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,13 +11,20 @@ public class hoverOverMethod {
 
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        driver = new ChromeDriver();
+
         driver.get("https://www.etsy.com/");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-        
+        WebElement jewelry = driver.findElement(By.xpath("//span[contains(text(),'Jewelry & Accessories')]"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(jewelry).perform();
+
+
 
     }
 }
