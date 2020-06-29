f583a838280848a7b4ca125e3c5c933f028d5071
Serdar <serdar2225@gmail.com>
2020-06-29 00:42:57 -0400

    handle window

--- src/main/java/handleWindow.java
+++ src/main/java/handleWindow.java
@@ -1,6 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class handleWindow {
@@ -9,10 +11,20 @@ public class handleWindow {
         WebDriver driver;
         driver = new ChromeDriver();
 
-        driver.get("http://demo.guru99.com/test/delete_customer.php");
+        driver.get("http://demo.guru99.com/popup.php");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+        driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+
+        Set<String> windowHandle = driver.getWindowHandles();
+
+        for(String window:windowHandle){
+            driver.switchTo().window(window);
+        }
+
+        driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys("blabla@gmail.com");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
     }
 }
