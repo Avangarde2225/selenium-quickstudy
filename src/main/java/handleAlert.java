40a55a29700344287ce0f5659f3bff5f957a808e
Serdar <serdar2225@gmail.com>
2020-06-28 22:34:32 -0400

    handle alert initial commit

---
+++ src/main/java/handleAlert.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class handleAlert {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+}
