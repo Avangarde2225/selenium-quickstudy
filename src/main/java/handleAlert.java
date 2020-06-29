891567cbeddaf5c32fe0a5b590dff2e357eb9c42
Serdar <serdar2225@gmail.com>
2020-06-28 22:54:29 -0400

    handle alert by using accept or dismiss()

--- src/main/java/handleAlert.java
+++ src/main/java/handleAlert.java
@@ -1,3 +1,5 @@
+import com.sun.tools.javac.util.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,9 +11,21 @@ public class handleAlert {
         WebDriver driver;
         driver = new ChromeDriver();
 
-        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+        driver.get("http://demo.guru99.com/test/delete_customer.php");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys("bla bla");
+        driver.findElement(By.xpath("//input[@name='submit']")).click();
+
+        driver.switchTo().alert().accept();
+
+        String textAlert = driver.switchTo().alert().getText();
+
+        System.out.println(textAlert);
+
+        driver.switchTo().alert().accept();
+
     }
 }
